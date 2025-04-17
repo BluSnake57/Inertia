9b91a644fe8387dfb8a058ed637638cbb94c4952
Scott Bissell <scotbisel@gmail.com>
2025-04-16 19:37:04 -0700

    Added navigation bar to go between pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,31 @@ import {Greet} from "../wailsjs/go/main/App";
 import Applications from './components/Applications';
 import Directories from './components/Directories';
 import Packages from './components/Packages';
+import Navbar from './components/Navbar';
 
 function App() {
+    const [page, setPage] = useState(Number)
+
+    function changePage(value:Number) {
+        switch(value) {
+            case 0:
+                return (<Applications/>);
+            case 1:
+                return (<Packages/>);
+            case 2:
+                return (<Directories/>);
+            default:
+                return (
+                    <label>Something went wrong</label>
+                )
+        }
+
+    }
 
     return (
         <div id="App">
-            <Applications/>
-            <Directories/>
-            <Packages/>
+            <Navbar selectedValue={page} setSelectedValue={setPage}/>
+            {changePage(page)}
         </div>
     )
 }
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,1 +1,52 @@
-import React from 'react';
+import React, { Component, Dispatch, ReactElement, SetStateAction, useState } from 'react';
+import Directories from './Directories';
+import Applications from './Applications';
+import Packages from './Packages';
+
+
+interface SelectedPage {
+    selectedValue: Number;
+    setSelectedValue: React.Dispatch<React.SetStateAction<number>>;
+  }
+
+function Navbar({selectedValue, setSelectedValue} : SelectedPage) {
+    const handleRadioChange = (value:number) => {
+        setSelectedValue(value);
+    };
+
+    return (
+        <nav className='navbar'>
+            <div>
+                <input type="radio" id="Applications" value={0}
+                    checked={ selectedValue === 0 }
+                    onChange={() =>
+                        handleRadioChange(0)
+                    }
+                />
+                <label>Applications</label>
+            </div>
+            <div>
+                <input type="radio" id="Packages" value={1}
+                    checked={ selectedValue === 1 }
+                    onChange={() =>
+                        handleRadioChange(1)
+                    }
+                />
+                <label>Packages</label>
+            </div>
+            <div>
+                <input type="radio" id="Directories" value={2}
+                    checked={ selectedValue === 2 }
+                    onChange={() =>
+                        handleRadioChange(2)
+                    }
+                />
+                <label>Directories</label>
+            </div>
+            
+        </nav>
+    )
+
+}
+
+export default Navbar
