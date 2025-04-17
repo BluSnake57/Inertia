e6a102bface6e90888c4da84a0311b6025fa395e
Scott Bissell <scotbisel@gmail.com>
2025-04-16 21:31:43 -0700

    Improved Application page usability

--- frontend/src/components/Applications.tsx
+++ frontend/src/components/Applications.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useContext, useState} from 'react';
 import {Get_App_List} from "../../wailsjs/go/main/App"
+import './Applications.css'
 
 
 interface App_List {
@@ -9,6 +10,7 @@ interface App_List {
 
 function Applications() {
     const [appList, setAppList] = useState(Array<string>)
+    const [selectedAppList, setSelectedAppList] = useState(Array<string>)
     const app_list: App_List = {selected_apps: []}
 
     function get_app_list() {
@@ -24,47 +26,41 @@ function Applications() {
         <div>
             <div>Select Apps from the list to backup</div>
             <button id="button" className='button' onClick={get_app_list}>Get Apps</button>
-            <table>
-                <tbody>
-                    {appList.map((item, index) => App_Element(item, index, app_list))}
-                </tbody>
-            </table>
-
             <button id="button" className='button' onClick={get_app_list}>Backup</button>
-            <table>
-                <tbody>
-                    {app_list.selected_apps.map((item, index) => App_Element(item, index, app_list))}
-                </tbody>
-            </table>
+            <div className='tables'>
+                <table className='unselected-table'>
+                    <label>Unselected</label>
+                    <tbody className='unselected-table-body'>
+                        {appList.map((item, index) => App_Element(item, index, appList, setAppList, selectedAppList, setSelectedAppList))}
+                    </tbody>
+                </table>
+                <line className='border'/>
+                <table className='selected-table'>
+                    <label>Selected</label>
+                    <tbody className='selected-table-body'>
+                        {selectedAppList.map((item, index) => App_Element(item, index, selectedAppList, setSelectedAppList, appList, setAppList))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
 
 
-function App_Element(app_name:string, index: number, app_list: App_List) {
+function App_Element(app_name:string, index: number, appList: Array<string>, setAppList: React.Dispatch<React.SetStateAction<string[]>>, selectedAppList: Array<string>, setSelectedAppList: React.Dispatch<React.SetStateAction<string[]>>) {
 
+    function selectionChange() {
+        setSelectedAppList([...selectedAppList, app_name])
+        appList = appList.filter(e => e !== app_name);
+        setAppList([...appList])
+    }
 
     return (
         <tr>
             <td key={index}>{app_name}</td>
-            <input key={index} type="checkbox"/>
+            <input key={index} type="button" onClick={() => selectionChange()}/>
         </tr>
     )
 }
-
-// function App_Elements(app_list:string[]) {
-
-//     function build_app_list() {
-//         app_list.forEach(function (app_name){
-//             App_Element(app_name)
-//         })
-//     }
-
-//     return (
-//         <div>
-//             {}
-//         </div>
-//     )
-// }
 
 export default Applications
