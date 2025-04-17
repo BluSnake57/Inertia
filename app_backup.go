0d2c19da77bc8910e246463ede68f68d3f3e81f5
Scott Bissell <scotbisel@gmail.com>
2025-04-17 13:25:39 -0700

    App backups now parse .desktop files and check for app data before returning backupable list

--- app_backup.go
+++ app_backup.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"slices"
@@ -8,21 +9,21 @@ import (
 )
 
 var app_directory = "/usr/share/applications"
+var home_directory = os.Getenv("HOME")
+var local_app_directories = []string{home_directory + "/.", home_directory + "/.local/share/"}
+var desktop_file_path_element = []byte("Exec=")
 
 // These extensions on a file will be removed to make it more readable
 var extensions_to_remove = []string{"com", "org"}
 
-// Any file with any of these strings in it will be ignored and not shown
-// Purpose being is these are apps are more system apps and should be covered
-// by directory backup
-var apps_to_ignore = []string{"freedesktop", "gnome", "ibus", "gtk3"}
+// Any file with any of these strings in it will be ignored and not shown. Purpose being is these are
+// apps are more system apps and should be covered by directory backup
+var apps_to_ignore = []string{"freedesktop", "gnome-software", "ibus", "gtk3"}
 
-// Don't need this at the moment but may be neccesary if I want to return a list of what was backed up and to where
-// It will also be neccesary when it comes to saving the metadata for the backup
-type App_Element struct {
+type AppElement struct {
 	Name         string `json:"name"`
-	Restore_Path string `json:"restore_path"`
 	Backup_Path  string `json:"backup_path"`
+	Restore_Path string `json:"restore_path"`
 }
 
 func (a *App) Get_App_List() []string {
@@ -32,28 +33,97 @@ func (a *App) Get_App_List() []string {
 		return nil
 	}
 
-	var app_list []string
+	var app_list []AppElement
+	var app_element AppElement
+	var app_name_list []string
 
 	for _, e := range apps {
-		app := strings.Split(e.Name(), "/")[0]
-		app_parts := strings.Split(app, ".")
 
 		for _, j := range apps_to_ignore {
-			if strings.Contains(app, j) {
+			if strings.Contains(e.Name(), j) {
+				log.Println("skipping")
 				goto Next
 			}
 		}
 
-		for _, j := range extensions_to_remove {
-			ext := slices.Index(app_parts, j)
-			if ext != -1 {
-				app_parts = slices.Delete(app_parts, ext, ext+1)
-			}
+		app_element = Get_App_Element(e.Name())
+
+		if app_element.Name != "" && !slices.Contains(app_list, app_element) {
+			app_list = append(app_list, app_element)
+			app_name_list = append(app_name_list, app_element.Name)
 		}
-		app_list = append(app_list, strings.Join(app_parts[:len(app_parts)-1], "."))
 
 	Next:
 	}
 
-	return app_list
+	log.Println(app_list)
+	return app_name_list
+}
+
+func Get_App_Element(file string) AppElement {
+	file_bytes, err := os.ReadFile(app_directory + "/" + file)
+	var appElement AppElement
+	if err != nil {
+		log.Println(err.Error())
+		return AppElement{}
+	}
+
+	bytes_matched := 0
+	compare_start_index := 0
+
+	for i, e := range file_bytes {
+		if e == desktop_file_path_element[bytes_matched] {
+			compare_start_index = i - bytes_matched
+			bytes_matched++
+		} else {
+			compare_start_index = 0
+			bytes_matched = 0
+		}
+
+		if bytes_matched == len(desktop_file_path_element) {
+			log.Println("matched bytes at index: " + fmt.Sprint(compare_start_index))
+			line := string(Get_Line(file_bytes[compare_start_index:]))
+			directory := strings.Split(line, "=")[1]
+			directory = strings.Split(directory, " ")[0]
+			log.Println(directory)
+			if strings.Contains(directory, "/") {
+				directory_parts := strings.Split(directory, "/")
+				directory = directory_parts[len(directory_parts)-1]
+			}
+			log.Println(directory)
+
+			for _, j := range local_app_directories {
+				_, err := os.Stat(j + directory)
+				if err != nil {
+					log.Println("can't find: " + j + directory)
+					continue
+				}
+
+				appElement.Restore_Path = j + directory
+			}
+
+			if appElement.Restore_Path == "" {
+				return AppElement{}
+			}
+			appElement.Name = directory
+			appElement.Backup_Path = backup_path + directory
+			log.Println(appElement)
+
+			break
+		}
+	}
+
+	return appElement
+}
+
+func Get_Line(data []byte) []byte {
+	var line []byte
+	for _, e := range data {
+		if e == byte(10) {
+			return line
+		}
+		line = append(line, e)
+	}
+
+	return []byte("")
 }
